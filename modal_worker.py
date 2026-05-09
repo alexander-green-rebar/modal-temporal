965b7af7e119e94b007722fc7c10192b62382ed8
Thomas J. Fan <thomas@modal.com>
2026-05-09 16:43:02 -0400

    Single file

--- modal_worker.py
+++ modal_worker.py
@@ -1,8 +1,12 @@
-from typing import Any, Callable
+import re
 import os
-import modal
-from temporalio.client import Client
+from datetime import timedelta
+from typing import Any
+from concurrent.futures import ThreadPoolExecutor
 
+import modal
+from temporalio import activity, workflow
+from temporalio.client import Client
 from temporalio.worker import Worker
 
 from modal_temporal import (
@@ -10,17 +14,13 @@ from modal_temporal import (
     DispatchInterceptor,
 )
 
-from workflows import SayHelloWorkflow
-from concurrent.futures import ThreadPoolExecutor
-from activities import greet, word_count
-
 APP_NAME = "temporal-testing"
 app = modal.App(APP_NAME)
 
 image = (
     modal.Image.debian_slim()
     .uv_pip_install("temporalio==1.27.0")
-    .add_local_python_source("activities", "workflows", "modal_temporal")
+    .add_local_python_source("modal_temporal")
 )
 
 
@@ -36,6 +36,11 @@ async def get_temporal_client() -> Client:
     )
 
 
+@activity.defn
+async def greet(name: str) -> str:
+    return f"Hello {name}"
+
+
 @app.function(env=env, image=image, cpu=0.5)
 async def greet_runner(args: Any, task_token: bytes) -> None:
     """Runs the `greet` activity.
@@ -45,12 +50,29 @@ async def greet_runner(args: Any, task_token: bytes) -> None:
     await run_activity(greet, args, client, task_token)
 
 
+@activity.defn
+def word_count(text: str) -> int:
+    return len(re.findall(r"\b[a-zA-Z]+\b", text))
+
+
 @app.function(env=env, image=image, cpu=1)
 async def word_count_runner(args: Any, task_token: bytes) -> None:
     """Runs the `word_count` activity.
     Note that the dispatcher assumes that the modal function is named `{activity_name}_runner`."""
     client = await get_temporal_client()
     await run_activity(word_count, args, client, task_token)
+
+
+@workflow.defn
+class SayHelloWorkflow:
+    @workflow.run
+    async def run(self, name: str) -> int:
+        result = await workflow.execute_activity(
+            greet, name, schedule_to_close_timeout=timedelta(seconds=30)
+        )
+        return await workflow.execute_activity(
+            word_count, result, schedule_to_close_timeout=timedelta(seconds=30)
+        )
 
 
 @app.function(schedule=modal.Period(minutes=30), timeout=30 * 60, image=image, env=env)
